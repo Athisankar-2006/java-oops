e2baf9e1073ed5040e95b21b3af339d440782d6b
Athisankar-2006 <athisankar2k6@gmail.com>
2025-10-22 21:17:27 +0530

    jagged array program added

---
+++ Arrays/JaggedArrayExample.java
@@ -0,0 +1,31 @@
+
+
+public class JaggedArrayExample {
+    public static void main(String[] args) {
+       
+        int[][] jagged = new int[3][];
+        jagged[0]=new int [2];
+        jagged[1]=new int [3];
+        jagged[2]=new int [4];
+
+       int value=1;
+        
+        for (int i=0;i<jagged.length;i++){
+            for (int j=0;j<jagged[i].length;j++){
+                jagged[i][j]=value++;
+            }
+        }
+
+        System.out.println("array element :");
+        for(int i=0;i<jagged.length;i++){
+            for(int j=0;j<jagged[i].length;j++){
+                System.out.println(jagged[i][j] +"\t");
+            }
+            System.out.println();
+        }
+
+        
+
+    }
+    
+}
--- Arrays/MatrixExample.java
+++ Arrays/MatrixExample.java
@@ -2,10 +2,89 @@ import java.util.Scanner;
 
 class MatrixExample {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        System.out.println(a);
+       Scanner sc= new Scanner(System.in);
+      
+
+       int[] [] arr=new int[3][3];
+
+
+ System.out.println("Enter 9 elemet for 3x3 matrix:");
+
+
+       for( int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+            arr[i][j]=sc.nextInt();
+        }
+       }
+
+
+       System.out.println("\n here the array element:");
+
+
+       for(int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+            System.out.println(arr[i][j]+ "\t");
+        }
+        System.out.println();
+       }
+
+
+       int sum=0;
+
+       for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            sum+=arr[i][j];
+        }
+       }
+       System.out.println("sum of all element:"+sum);
+          
+     
+        sc.close();
+      
     }
 
     
 }
+
+
+
+
+// import java.util.Scanner;
+
+// public class MatrixExample {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+
+//         // Create a 3x3 matrix
+//         int[][] matrix = new int[3][3];
+
+//         // Input elements
+//         System.out.println("Enter 9 elements for the 3x3 matrix:");
+//         for (int i = 0; i < 3; i++) {
+//             for (int j = 0; j < 3; j++) {
+//                 matrix[i][j] = sc.nextInt();
+//             }
+//         }
+
+//         // Display the matrix
+//         System.out.println("\nMatrix is:");
+//         for (int i = 0; i < 3; i++) {
+//             for (int j = 0; j < 3; j++) {
+//                 System.out.print(matrix[i][j] + "\t");
+//             }
+//             System.out.println();
+//         }
+
+//         // Calculate the sum of all elements
+//         int sum = 0;
+//         for (int i = 0; i < 3; i++) {
+//             for (int j = 0; j < 3; j++) {
+//                 sum += matrix[i][j];
+//             }
+//         }
+
+//         System.out.println("\nSum of all elements: " + sum);
+
+//         sc.close();
+//     }
+// }
