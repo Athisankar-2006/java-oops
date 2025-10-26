bacdd5985a33969ec07c3564418a475ebf950dd0
Athisankar-2006 <athisankar2k6@gmail.com>
2025-10-26 11:01:56 +0530

    array intermediate comlpeted

---
+++ Arrays/MatrixAdditionTranspose.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class MatrixAdditionTranspose {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        int[][] a=new int[2][2];
+        int[][] b=new int[2][2];
+        int[][] sum=new int[2][2];
+        int[][] transpose=new int[2][2];
+        
+        System.out.println("Enter elements of first 2x2 matrix (A):");
+
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                a[i][j]=sc.nextInt();
+            }
+        }
+    
+
+        System.out.println("Enter elements of first 2x2 matrix (B):");
+
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                b[i][j]=sc.nextInt();
+            }
+        }
+
+
+        //sum of two array
+
+        // System.out.println("Addition of arrays (A+B):");
+
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                 sum[i][j]=a[i][j]+b[i][j];
+            }
+        }
+
+
+        //transpose of sum
+        
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+                transpose[i][j]= sum[j][i];
+            }
+        }
+
+        //display all array a,b, sum,transpose
+
+
+        System.out.println("Matrix A:");
+        printMatrix(a);
+
+        System.out.println("Matrix B:");
+        printMatrix(b);
+           
+        System.out.println("Matrix SUM:");
+        printMatrix(sum);
+         
+        System.out.println("Matrix TRANSPOSE:");
+        printMatrix(transpose);
+
+        sc.close();
+
+    }
+        //helper methods to print 2d array
+
+        public static void printMatrix(int[][] mat){
+            for(int i=0;i<2;i++){
+                for(int j=0;j<2;j++){
+                    System.out.print(mat[i][j] +"\t");
+                }
+                System.out.println();
+         
+            }
+        
+        
+        
+        }
+
+
+    
+    
+}
---
+++ Arrays/RemoveDuplicates.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter size of array:");
+        int n=sc.nextInt();
+
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            System.out.print("Enter"+(i+1)+" st element:");
+            arr[i]=sc.nextInt();
+        }
+
+        // create another object to store element without duplicate
+
+        int[] temp=new int[n];
+        int j=0;
+
+        for(int i=0;i<n-1; i++){
+            if(arr[i]!= arr[i+1]){
+                temp[j++]=arr[i++];
+            }
+        }
+        //add the last element
+        temp[j++]=arr[n-1];
+
+        //display the unique element
+
+        System.out.println("the unique element:");
+        for(int i=0; i<n; i++){
+            System.out.print(temp[i] +" ");
+        }
+        sc.close();
+    }
+}
